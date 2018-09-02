7e93c36fcfbbaa2986485bfaeb02d63969b148d6
rkb94 <sfo740c.john@gmail.com>
2018-09-02 13:19:57 +0900

    1247 update

--- 1247/1247.py
+++ 1247/1247.py
@@ -1,5 +1,15 @@
-ansewr=[]
-
-
+answer=[]
 for i in range(3):
-    number = input()
+    input_num = input()
+    mylist = [0]*input_num
+    for j in range(len(mylist)):
+        mylist[j] = input()
+    answer.append('0' if sum(mylist) == 0 else '-' if sum(mylist)<0 else '+')
+print ('\n'.join(answer))
+
+# test
+# answer=[]
+# for i in range(3):
+#     s = sum([int(raw_input()) for j in range(int(raw_input()))])
+#     answer.append('0' if s==0 else '-' if s<0 else '+')
+# print ('\n'.join(answer))
