a20915ac561d2098e8cb7ef38566d1be198c9d3d
Kyungbin Ryu <rkbin@hayanmind.com>
2019-02-16 18:48:58 +0900

    Update 1021

--- 1021/main.cpp
+++ 1021/main.cpp
@@ -16,32 +16,32 @@ int main() {
 
     int count = 0;
     for (int j = 0; j < M; ++j) {
-        int now;
+        int now; // 지금 찾으려는 입력 된 값
         cin >> now;
         int size = deq.size();
-        int index = 1;
+        int index = 1; // 처음 선택되어 있는 인덱스..
 
         for (iter = deq.begin(); iter < deq.end(); iter++) {
             if (*iter == now)break;
             index++;
         }
 
-        int left = index - 1;
-        int right = size - index +1;
-        if (left < right){
-            for (int i = 1; i <= left; ++i) {
+        int left = index - 1; // 왼쪽으로 접근했을 때 얼마나 먼가?
+        int right = size - index +1; // 오른쪽으로 접근했을 때 얼마나 먼가?
+        if (left < right){ // 둘이 비교했을 때 뭐가 더 가까운가?
+            for (int i = 1; i <= left; ++i) { // 왼쪽으로 접근했을 떄 index를 몇 번 이동해야하는지..
                 deq.push_back(deq.at(0));
                 deq.pop_front();
                 count++;
             }
-            deq.pop_front();
+            deq.pop_front(); // deq에서 원하는 원소를 뽑아냄
         } else {
-            for (int i = 1; i <= right; ++i) {
+            for (int i = 1; i <= right; ++i) { // 오른 쪽으로 접근했을 때 index를 몇 번 이동해야 하는지..
                 deq.push_front(deq.at(size-1));
                 deq.pop_back();
                 count++;
             }
-            deq.pop_front();
+            deq.pop_front(); // deq에서 원하는 원소를 뽑아냄
         }
     }
     cout << count << endl;
