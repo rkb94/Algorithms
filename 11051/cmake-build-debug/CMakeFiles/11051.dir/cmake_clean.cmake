8a25eff08b139581e7ee73027866412e481a86d4
Kyungbin Ryu <rkbin@hayanmind.com>
2019-02-17 18:19:07 +0900

    Add 11050, 11051

---
+++ 11051/cmake-build-debug/CMakeFiles/11051.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11051.dir/main.cpp.o"
+  "11051.pdb"
+  "11051"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11051.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
