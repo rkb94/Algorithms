1bbfb61edc73183707b99e04c0d07599d5d63d8a
Kyungbin Ryu <rkbin@hayanmind.com>
2019-02-26 03:05:02 +0900

    Update 9251

--- 9251/main.cpp
+++ 9251/main.cpp
@@ -8,8 +8,8 @@ int main() {
     string text2;
     int dp[1001][1001] = {0};
 
-    cin >> text1;
     cin >> text2;
+    cin >> text1;
 
     text1 = "0" + text1;
     text2 = "0" + text2;
@@ -18,7 +18,13 @@ int main() {
 
     for (i = 1; i < text1.length(); ++i) {
         for (j = 1; j < text2.length(); ++j) {
-            dp[i][j] = max({dp[i][j-1], dp[i-1][j], dp[i-1][j-1] + (text1[i] == text2[j])});
+            if (text1[i] == text2[j]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            } else {
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+//            dp[i][j] = max({dp[i][j-1], dp[i-1][j], dp[i-1][j-1] + (text1[i] == text2[j])});
+            //  이것도 맞는 내용.. 간결하게 코드를짜자..
         }
     }
 
